fc30a32d9111b3ede7852ea6d3c2e8fe0135a68d
tetsu <69633379+tetsunari@users.noreply.github.com>
2025-07-22 14:40:54 +0900

    Update function.zsh

--- private_dot_config/zsh/function.zsh
+++ private_dot_config/zsh/function.zsh
@@ -10,15 +10,6 @@ export PATH="$CHEZMOI_HOME:/chezmoi:$PATH"
 autoload bashcompinit && bashcompinit
 autoload -Uz compinit && compinit
 
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
-export DefaultIMModule=fcitx
-if [ $SHLVL = 1 ] ; then
-  (fcitx-autostart > /dev/null 2>&1 &)
-  xset -r 49  > /dev/null 2>&1
-fi
-
 # code コマンド
 export PATH=$PATH:"$(wslpath $(/mnt/c/WINDOWS/system32/cmd.exe /c 'SET /P X=%USERPROFILE%<NUL' 2>/dev/null))/AppData/Local/Programs/Microsoft VS Code/bin"
 # cursor コマンド
