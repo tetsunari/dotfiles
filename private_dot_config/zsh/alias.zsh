96cb4b353fe6ca3d65ce762fb243233d4d273864
Tetsuya Matsushita <matsushita_te@micware.co.jp>
2025-07-24 12:07:41 +0900

    add claude desktop command

--- private_dot_config/zsh/alias.zsh
+++ private_dot_config/zsh/alias.zsh
@@ -1,13 +1,17 @@
-alias ls='eza --group-directories-first --time-style=long-iso --git'
 alias lg='lazygit'
 alias vim='nvim'
 alias vi='nvim'
 alias wsl='/mnt/c/Windows/System32/wsl.exe'
+# Notion
 alias notion="nohup $(wslpath $(/mnt/c/WINDOWS/system32/cmd.exe /c 'SET /P X=%USERPROFILE%<NUL' 2>/dev/null))/AppData/Local/Programs/Notion/Notion.exe > /dev/null 2>&1"
 alias notkill='/mnt/c/Windows/System32/taskkill.exe /F /IM Notion.exe'
+# Claude Desktop
+alias dt-claude="nohup $(wslpath $(/mnt/c/WINDOWS/system32/cmd.exe /c 'SET /P X=%USERPROFILE%<NUL' 2>/dev/null))/AppData/Local/Application\ Data/AnthropicClaude/claude.exe > /dev/null 2>&1"
+alias dt-claudekill='/mnt/c/Windows/System32/taskkill.exe /F /IM claude.exe'
 
 # Modern CLI Tools (Homebrew)
 alias cat='bat'
+alias ls='eza --group-directories-first --time-style=long-iso --git'
 alias ll='eza -l --group-directories-first --time-style=long-iso --git'
 alias la='eza -a --group-directories-first'
 alias tree='eza --tree'
