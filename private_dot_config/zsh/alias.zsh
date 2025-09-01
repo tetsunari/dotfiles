10df5c62476478a74fb332d85a00d93263eb8b57
Tetsuya Matsushita <matsushita_te@micware.co.jp>
2025-09-01 19:08:53 +0900

    fix alias

--- private_dot_config/zsh/alias.zsh
+++ private_dot_config/zsh/alias.zsh
@@ -3,10 +3,10 @@ alias vim='nvim'
 alias vi='nvim'
 alias wsl='/mnt/c/Windows/System32/wsl.exe'
 # Notion
-alias notion="nohup $(wslpath $(/mnt/c/WINDOWS/system32/cmd.exe /c 'SET /P X=%USERPROFILE%<NUL' 2>/dev/null))/AppData/Local/Programs/Notion/Notion.exe > /dev/null 2>&1"
+alias notion="nohup $(wslpath $(/mnt/c/WINDOWS/system32/cmd.exe /c 'SET /P X=%USERPROFILE%<NUL' 2>/dev/null))/AppData/Local/Programs/Notion/Notion.exe > /dev/null 2>&1 &"
 alias notkill='/mnt/c/Windows/System32/taskkill.exe /F /IM Notion.exe'
 # Claude Desktop
-alias dt-claude="nohup $(wslpath $(/mnt/c/WINDOWS/system32/cmd.exe /c 'SET /P X=%USERPROFILE%<NUL' 2>/dev/null))/AppData/Local/Application\ Data/AnthropicClaude/claude.exe > /dev/null 2>&1"
+alias dt-claude="nohup $(wslpath $(/mnt/c/WINDOWS/system32/cmd.exe /c 'SET /P X=%USERPROFILE%<NUL' 2>/dev/null))/AppData/Local/Application\ Data/AnthropicClaude/claude.exe > /dev/null 2>&1 &"
 alias dt-claudekill='/mnt/c/Windows/System32/taskkill.exe /F /IM claude.exe'
 
 # Modern CLI Tools (Homebrew)
