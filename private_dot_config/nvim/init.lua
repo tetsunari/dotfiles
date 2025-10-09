0b4bc3ac270ac13f953da62769e2798def4ce8c2
Tetsuya Matsushita <matsushita_te@micware.co.jp>
2025-10-09 10:43:20 +0900

    update nvim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,4 +1,17 @@
+require 'function'
 require 'keybinds'
 require 'plugin'
 require 'options'
+
+vim.opt.clipboard:append('unnamedplus,unnamed')
+
+vim.opt.whichwrap = 'b,s,h,l,<,>,[,],~'
+
+vim.api.nvim_create_user_command(
+  'InitLua',
+  function()
+    vim.cmd.edit(vim.fn.stdpath('config') .. '/init.lua')
+  end,
+  { desc = 'Open init.lua' }
+)
 
--- private_dot_config/nvim/lua/keybinds.lua
+++ private_dot_config/nvim/lua/keybinds.lua
@@ -11,3 +11,16 @@ vim.keymap.set('n', 's', '"_s')
 
 vim.api.nvim_set_var('mapleader', ' ')
 
+-- https://zenn.dev/kawarimidoll/books/6064bf6f193b51/viewer/6c77c3
+vim.keymap.set('n', 'p', 'p]', { desc = 'Paste and move to the end' })
+vim.keymap.set('n', 'P', 'P]', { desc = 'Paste and move to the end' })
+
+vim.keymap.set('x', 'p', 'P', { desc = 'Paste without change register' })
+vim.keymap.set('x', 'P', 'p', { desc = 'Paste with change register' })
+
+-- { 'n', 'x' }とすると複数のモードに対応
+vim.keymap.set('n', 'X', '"_D', { desc = 'Delete using blackhole register' })
+vim.keymap.set('o', 'x', 'd', { desc = 'Delete using x' })
+
+vim.keymap.set('n', '<c-s>', '<cmd>write<cr>', { desc = 'Write' })
+vim.keymap.set({ 'n', 'x' }, 'so', ':source<cr>', { silent = true, desc = "Source crrent script" })
