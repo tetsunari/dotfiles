a7947734fbb881132257fd6099ebbec58784dd11
tetsunari <mtst.tty556@gmail.com>
2026-01-08 09:03:16 +0900

    add yazi

---
+++ private_dot_config/yazi/init.lua
@@ -0,0 +1,38 @@
+function Linemode:p_s_m()
+    local cha = self._file.cha
+
+    -- 1. permissions
+    -- Unix系では cha:perm() が "rwxrwxrwx" 形式を返します
+    -- 取れない場合は "----------" をデフォルトにします
+    local permi = (type(cha.perm) == "function" and cha:perm()) 
+               or (type(cha.permissions) == "function" and cha:permissions())
+               or "----------"
+
+    -- 2. size
+    local size = self._file:size()
+    local size_str = size and ya.readable_size(size) or "- "
+
+    -- 3. time
+    local time_str = ""
+    if cha.mtime then
+        local time = math.floor(cha.mtime)
+        if os.date("%Y", time) == os.date("%Y") then
+            time_str = os.date("%b %d %H:%M", time)
+        else
+            time_str = os.date("%b %d  %Y", time)
+        end
+    end
+
+    -- 4. 描画
+    -- パーミッションが長すぎて隠れないよう、右詰めにせず順番に並べます
+    return ui.Line({
+        -- ui.Span(permi):style(ui.Style():fg("cyan")),
+        -- ui.Span(" "),
+        -- ui.Span(string.format("%7s", size_str)):style(ui.Style():fg("yellow")),
+        -- ui.Span(" "),
+        -- ui.Span(time_str):style(ui.Style():fg("white")),
+        ui.Span(permi),
+        ui.Span(string.format("%7s ", size_str)):style(ui.Style():fg("yellow")),
+        ui.Span(time_str),
+    })
+end
