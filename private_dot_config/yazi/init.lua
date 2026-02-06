10386ace204fca71355b7704afc9e35bd7f6138e
tetsunari <mtst.tty556@gmail.com>
2026-02-06 09:36:12 +0900

    update yazi

--- private_dot_config/yazi/init.lua
+++ private_dot_config/yazi/init.lua
@@ -1,18 +1,26 @@
+require("full-border"):setup({
+  type = ui.Border.ROUNDED,
+})
+require("zoxide"):setup({
+  update_db = true,
+})
+require("git"):setup()
+
 function Linemode:p_s_m()
     local cha = self._file.cha
 
-    -- 1. permissions
+    -- permissions
     -- Unix系では cha:perm() が "rwxrwxrwx" 形式を返します
     -- 取れない場合は "----------" をデフォルトにします
     local permi = (type(cha.perm) == "function" and cha:perm()) 
                or (type(cha.permissions) == "function" and cha:permissions())
                or "----------"
 
-    -- 2. size
+    -- size
     local size = self._file:size()
     local size_str = size and ya.readable_size(size) or "- "
 
-    -- 3. time
+    -- time
     local time_str = ""
     if cha.mtime then
         local time = math.floor(cha.mtime)
@@ -23,7 +31,7 @@ function Linemode:p_s_m()
         end
     end
 
-    -- 4. 描画
+    -- 描画
     -- パーミッションが長すぎて隠れないよう、右詰めにせず順番に並べます
     return ui.Line({
         -- ui.Span(permi):style(ui.Style():fg("cyan")),
