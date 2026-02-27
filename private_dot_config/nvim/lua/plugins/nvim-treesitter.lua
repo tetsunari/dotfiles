a9279dd62dd0ad4c0dac23240ec1861ac0095e4e
tetsunari <mtst.tty556@gmail.com>
2026-02-27 11:10:18 +0900

    nvim plugin nvim-treesitter update

--- private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -4,47 +4,59 @@ return {
   event = { "BufNewFile", "BufRead" },
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter").setup({
-      ensure_installed = {
-        "bash",
-        "css",
-        "diff",
-        "dockerfile",
-        "elixir",
-        "gitignore",
-        "gleam",
-        "go",
-        "html",
-        "http",
-        "javascript",
-        "json",
-        "kdl",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "rust",
-        "scss",
-        "sql",
-        "svelte",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "vue",
-        "yaml",
-      },
-      sync_install = false,
-      auto_install = true,
-      ignore_install = {},
-      modules = {},
-      highlight = {
-        enable = true,
-        disable = { "ini" },
-      },
-      -- texobjectsはパフォーマンスの問題から利用しない
+    require("nvim-treesitter").setup()
+
+    -- iniファイルのみtreesitterハイライトを無効化
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "ini",
+      callback = function(args)
+        pcall(vim.treesitter.stop, args.buf)
+      end,
     })
+
+    -- 未インストールのパーサーを自動インストール
+    local wanted = {
+      "bash",
+      "css",
+      "diff",
+      "dockerfile",
+      "elixir",
+      "gitignore",
+      "gleam",
+      "go",
+      "html",
+      "http",
+      "javascript",
+      "json",
+      "kdl",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "rust",
+      "scss",
+      "sql",
+      "svelte",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "vue",
+      "yaml",
+    }
+    local installed = require("nvim-treesitter.config").get_installed("parsers")
+    local missing = vim.tbl_filter(function(p)
+      return not vim.list_contains(installed, p)
+    end, wanted)
+    if #missing > 0 then
+      vim.schedule(function()
+        vim.cmd("TSInstall " .. table.concat(missing, " "))
+      end)
+    end
   end,
-  vim.treesitter.language.register("bash", "zsh"),
+  init = function()
+    -- zshファイルにbashパーサーを使用
+    vim.treesitter.language.register("bash", "zsh")
+  end,
 }
